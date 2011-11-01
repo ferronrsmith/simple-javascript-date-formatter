25d9c5e6ca0432a914bff313c75bf478c764a5c0
ferronrsmith <ferronrsmith@gmail.com>
2011-11-01 02:50:46 +0000

    fixed grammatical errors

--- date.js
+++ date.js
@@ -13,6 +13,7 @@ var gsMonthNames = new Array(
 'January','February','March','April','May','June','July','August','September','October','November','December'
 );
 
+
 // a global day names array
 var gsDayNames = new Array(
 'Sunday','Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'
@@ -28,7 +29,7 @@ Date.prototype.format = function(f)
 
     var d = this;
 
-    return f.replace(/(yy|yyyy|mmmm|mmm|mm|dddd|ddd|dd|hh|nn|ss|a\/p)/gi,
+    return f.replace(/(yy|yyyy|mmmm|mmm|mm|dddd|ddd|dd|hh|mm|ss|a\/p)/gi,
         function(val)
         {
             switch (val.toLowerCase())
@@ -42,7 +43,7 @@ Date.prototype.format = function(f)
 				case 'ddd':  return gsDayNames[d.getDay()].substr(0, 3);
 				case 'dd':   return d.getDate().zf(2);
 				case 'hh':   return ((h = d.getHours() % 12) ? h : 12).zf(2);
-				case 'nn':   return d.getMinutes().zf(2);
+				case 'mm':   return d.getMinutes().zf(2);
 				case 'ss':   return d.getSeconds().zf(2);
 				case 'a/p':  return d.getHours() < 12 ? 'am' : 'pm';
             }
